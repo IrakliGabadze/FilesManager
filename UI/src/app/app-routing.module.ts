988bcf46339a12daf335010917ae9e3abc9327a9
Irakli Gabadze <i.gabadze@altasoft.ge>
2023-10-04 17:13:46 +0400

    simple structure + toolbar icon

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './modules/files/pages/home/home.component';
-import { FilesComponent } from './modules/files/pages/files/files.component';
-import { AboutComponent } from './modules/files/pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FilesComponent } from './pages/files/files.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,26 +4,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { FilesModule } from './modules/files/files.module';
 import { AppRoutingModule } from './app-routing.module';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FolderItemComponent } from './components/folder-item.component';
+import { FilesComponent } from './pages/files/files.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
-    bootstrap:    [AppComponent],
-    declarations: [AppComponent],
-    imports:      [
+    bootstrap: [AppComponent],
+    declarations: [
+      AppComponent,
+      FolderItemComponent,
+      FilesComponent,
+      HomeComponent,
+      AboutComponent],
+    imports: [
       RouterModule,
       AppRoutingModule,
       BrowserModule,
       BrowserAnimationsModule,
-      FilesModule,
       HttpClientModule,
       MatToolbarModule,
       MatButtonModule,
       MatIconModule
     ]
-})
-export class AppModule {
-}
+  })
+  export class AppModule {
+  }
